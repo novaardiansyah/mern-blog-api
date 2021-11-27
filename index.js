54af9bbf681105787cdac240298e2992151f6e4a
novaardiansyah <novaardiansyah78@gmail.com>
2021-11-27 10:25:28 +0700

    troubleshooting error cors
    - allows the backend to skip the cors origin process

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const port = 3001
 
 const productsRoutes = require('./src/routes/products')
 
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+//   )
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+//   next()
+// })
+
 app.use('/', productsRoutes)
 
 app.listen(port)
